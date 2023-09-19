f38efbe814f4f185a21a2c1a7688eedd0a587047
cinta <cintatafur@gmail.com>
2023-09-19 11:52:56 +0200

    💚 Create(componentes):nav y header en módulo core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, NavComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, NavComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
