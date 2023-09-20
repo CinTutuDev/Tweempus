4594f92ced2cbe92f37be824802e05021c3a4915
cinta <cintatafur@gmail.com>
2023-09-20 11:44:35 +0200

    👽️See(data binding):paso datos mediante data binding y visualizo en servidor

--- src/app/shared/twimp/twimp-card/twimp-card.component.ts
+++ src/app/shared/twimp/twimp-card/twimp-card.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
-
+import { TwimpModel } from '../twimp.model';
 @Component({
   selector: 'tweempus-twimp-card',
   templateUrl: './twimp-card.component.html',
   styleUrls: ['./twimp-card.component.css'],
 })
 export class TwimpCardComponent {
-  @Input() timp!: any;
+  @Input() twimp!: TwimpModel;
 }
--- src/app/shared/twimp/twimp-list/twimp-list.component.ts
+++ src/app/shared/twimp/twimp-list/twimp-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TwimpModel } from '../twimp.model';
+import { AuthorModel } from '../../author/author.model';
 @Component({
   selector: 'tweempus-twimp-list',
   templateUrl: './twimp-list.component.html',
-  styleUrls: ['./twimp-list.component.css']
+  styleUrls: ['./twimp-list.component.css'],
 })
-export class TwimpListComponent {
+export class TwimpListComponent implements OnInit {
+  /*  crear array de twimps y para ello le tenemos que pasar un author/array*/
+  authors: AuthorModel[] = [];
+  text: string =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitursodales libero, sit amet posue';
+  twimps: TwimpModel[] = [];
 
+  ngOnInit(): void {
+    this.authors.push(new AuthorModel('1'));
+    this.twimps.push(
+      /*🗳Le pasamos los componentes del modelo a los twinps: _id;_url; _author; _content; timestamp;favorite*/
+      new TwimpModel('1', '', this.authors[0], this.text, '01/01/2023')
+    );
+    this.twimps.push(
+      new TwimpModel('2', '', this.authors[0], this.text, '01/01/2023')
+    );
+    this.twimps.push(
+      new TwimpModel('3', '', this.authors[0], this.text, '01/01/2023')
+    );
+    this.twimps.push(
+      new TwimpModel('4', '', this.authors[0], this.text, '01/01/2023')
+    );
+  }
 }
