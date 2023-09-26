fcf621c30d2160abd683fd28765b42183b8e197a
cinta <cintatafur@gmail.com>
2023-09-26 16:57:28 +0200

    💚 Service(concumiendo servicios Observable):🥅 catchError, Observable, subscribe...

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthorService } from '../shared/author/author.service';
 
 @Component({
   selector: 'tweempus-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  constructor(private authorS: AuthorService) {}
+  ngOnInit(): void {
+    this.authorS.getAuthor('1').subscribe((author) => console.log(author));
+  }
 }
--- src/app/shared/author/author.service.ts
+++ src/app/shared/author/author.service.ts
@@ -1,12 +1,41 @@
 import { Injectable } from '@angular/core';
 /* importaciones básicas para peticiones http */
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { AuthorModel } from './author.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthorService {
-  constructor() {}
+  private url: string = 'http://localhost:3000/authors';
+
+  getAuthor(id: string): Observable<AuthorModel> {
+    /* da error porque lo inicializo a null y luego le doy valor en el map  */
+    /* let author: AuthorModel = null; */
+
+    return this.http.get<AuthorModel>(this.url + '/' + id).pipe(
+      map((dbAuthor) => {
+        const author = new AuthorModel(dbAuthor.id);
+        author.fullName = dbAuthor.fullName;
+        author.image = dbAuthor.image;
+        author.url = 'http://localhost:4200/author/' + dbAuthor.id;
+        return author;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error: any) {
+    let errMsg = error.message
+      ? error.message
+      : error.status
+      ? `${error.status} - ${error.statusText}`
+      : 'Server error';
+    console.error(errMsg);
+    return throwError(() => errMsg);
+  }
+
+  constructor(private http: HttpClient) {}
 }
