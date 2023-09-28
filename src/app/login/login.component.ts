0ed6fc67de144ae5817c4f59b014d1d76ae3ba33
cinta <cintatafur@gmail.com>
2023-09-28 11:50:15 +0200

    Add(formulario de login):Reactive Forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../core/authentication.service';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { AuthorService } from '../shared/author/author.service';
 
 @Component({
   selector: 'tweempus-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  constructor(private authenticationS: AuthenticationService) {}
+export class LoginComponent implements OnInit {
+  /*  nuevo user */
+  userForm!: FormGroup;
+  userNoExist: boolean = false;
 
-  logIn() {
-    this.authenticationS.login('1');
+  constructor(
+    private authenticationS: AuthenticationService,
+    private authorS: AuthorService,
+    private FormB: FormBuilder
+  ) {}
+  ngOnInit(): void {
+    this.userForm = this.FormB.group({
+      idAuthor: ['', Validators.required],
+    });
+  }
+
+  logIn(form: any) {
+    if (this.userNoExist) {
+      this.userNoExist = false;
+    }
+    this.authorS.getAuthor(form.value.idAuthor).subscribe(
+      (author) => {
+        console.log('Author:', author);
+        this.authenticationS.login(form.value.idAuthor);
+      },
+      (error) => {
+        console.error('Error:', error);
+        this.userNoExist = true;
+      }
+    );
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -6,9 +6,15 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
 import { TranslocoRootModule } from '../transloco-root.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, LoginRoutingModule, TranslocoRootModule],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    TranslocoRootModule,
+    ReactiveFormsModule,
+  ],
   declarations: [LoginComponent],
 })
 export class LoginModule {}
