c343c8e709d11d408144789b39ddccaec73406db
cinta <cintatafur@gmail.com>
2023-11-07 15:21:32 +0100

    💄 styles more

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tweempus-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
 })
 export class ErrorComponent {
 
