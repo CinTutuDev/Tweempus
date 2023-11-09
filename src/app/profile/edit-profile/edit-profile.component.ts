fb7056107124ed34f9c620c36fe1988094eb416d
cinta <cintatafur@gmail.com>
2023-11-09 11:55:08 +0100

    ♻️ Refactorizando y modificando profile

--- src/app/profile/edit-profile/edit-profile.component.ts
+++ src/app/profile/edit-profile/edit-profile.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 import { AuthenticationService } from '../../core/authentication.service';
 import { AuthorService } from '../../shared/author/author.service';
@@ -12,14 +14,15 @@ import { Author } from '../../shared/author/author.model';
   styleUrls: ['./edit-profile.component.scss']
 })
 export class EditProfileComponent implements OnInit {
-
+  @Input() author: Author | null = null;
   editUserForm!: FormGroup;
-  currentAuthor: Author | null = null;
+ /*  currentAuthor: Author | null = null; */
 
   constructor(
     private authService: AuthenticationService,
     private authorService: AuthorService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private router: Router,) { }
 
   ngOnInit() {
     this.editUserForm = this.fb.group({
@@ -31,6 +34,18 @@ export class EditProfileComponent implements OnInit {
   editProfile(form: any) {
     this.authorService.updateAuthor(this.authService.token!.idAuthor, form.value.fullName, form.value.image).subscribe(
       response => console.log('Usuario modificado')
-    )
+    );
+      this.router.navigate(['/dashboard'])
+
   }
+
+  updatePreview() {
+    const imageUrl = this.editUserForm.get('image')!.value;
+    // Actualizar la vista previa solo si se proporciona una URL válida
+    if (imageUrl) {
+      this.editUserForm.patchValue({ image: imageUrl }, { emitEvent: false });
+    }
+  }
+
+
 }
